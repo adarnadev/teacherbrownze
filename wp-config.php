6e527972d7aee3a93da64c8cb6f15a2a5e798c73
Agno Almario <agnoalmario@Agnos-MacBook-Pro.local>
2014-02-19 14:16:05 +0800

    config edit for DB

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'ahdigital');
+define('DB_NAME', 'cdb_179a920468');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'b8912ce4c6bd4b');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'd05209a9');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'ap-cdbr-azure-east-b.cloudapp.net');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
