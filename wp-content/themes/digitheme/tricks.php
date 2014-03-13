be1be25d9b2b2d1305418ff15660fda6d918ccf5
Agno Almario <agnoalmario@Agnos-MacBook-Pro.local>
2014-03-13 12:01:42 +0800

    tricks

---
+++ wp-content/themes/digitheme/tricks.php
@@ -0,0 +1,29 @@
+<?php get_header() ?>
+<h1 class="niceh"> CSS Tricks! </h1>
+
+
+<h3 class="niceh"> Border </h3>
+<p>
+	Add a border to your html! <br>
+	Use this syntax:<br>
+	border: &lt;border size&gt; &lt;border type&gt; &lt;border color&gt; 
+ </p>
+
+
+<h4 class="niceh"> Example: </h4>
+
+<p> if I type this: <br>
+	border:1px solid red; <br>
+	i get this:
+</p>
+
+<p style="border:1px solid red">I have a border <p>
+
+<p> if I type this: <br>
+	border:1px dotted red; <br>
+	i get this:
+</p>
+
+<p style="border:1px dotted red">I have a dotted border <p>
+
+<?php get_footer() ?>
